121c559bfe67895087373975b3d96c8f0f12501f
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-04-12 11:39:31 -0400

    added image services

---
+++ src/services/image.js
@@ -0,0 +1,24 @@
+    
+export default ImageService = {};
+
+ImageService.init = () => {
+  if (localStorage.getItem('images') === null) {
+    localStorage.setItem('images', JSON.stringify([]));
+  }
+}
+
+ImageService.getImages = () => {
+  const imageArrayString = localStorage.getItem('images');
+  const imageArray = JSON.parse(imageArrayString);
+  return imageArray;
+}
+
+ImageService.saveImage = (url, timestamp) => {
+  const newImage = { url, timestamp };
+  const oldImages = ImageService.getImages();
+
+  oldImages.unshift(newImage);
+
+  localStorage.setItem('images', JSON.stringify(oldImages));
+  return oldImages;
+}
